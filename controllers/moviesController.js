01da999db5ff0634c26bae09d3ee601f57ce1dd8
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-04-15 14:39:36 +0200

    feat: implement show function to retrieve movie details and associated reviews

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -10,6 +10,35 @@ function index(req, res) {
     })
 }
 
+function show(req, res) {
+
+    const { id } = req.params
+
+    const movieSql = 'SELECT * FROM movies WHERE id = ?'
+    const reviewSql = 'SELECT * FROM reviews WHERE movie_id = ?'
+
+    connection.query(movieSql, [id], (err, result) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        if (result.length === 0) return res.status(404).json({ error: 'Movie not found' });
+
+        const movie = result[0]
+
+        connection.query(reviewSql, [id], (err, reviews) => {
+            if (err) return res.status(500).json({ error: 'Review not found' });
+
+            const reviewsData = reviews.map(review => {
+                const { id, movie_id, ...rest } = review;
+                return rest
+            })
+
+            movie.review = reviewsData
+
+            res.json(movie)
+        })
+    })
+}
+
 module.exports = {
-    index
+    index,
+    show
 }
